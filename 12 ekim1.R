a31aa85ffb304ead3bc7b1ee012df97c7a3cd766
Fatma <123814832+fatmaakpunar@users.noreply.github.com>
2023-10-13 07:07:34 +0300

    Add files via upload

---
+++ 12 ekim1.R
@@ -0,0 +1,23 @@
+minutes <- 60*24*365
+minutes
+
+60+24
+minutes <- 60 * 24 * 365
+minutes
+
+two.to.ten <- seq(from = 2, to = 10, by = 1)
+two.to.ten
+
+five.fours <- rep(4, times=5)
+five.fours
+seq12 = 100:200;mean(seq12);
+
+v <- 1:10
+y <- 2:11
+
+v*y
+
+c(v, y)
+
+rbind(v, y)
+cbind(v, y)
